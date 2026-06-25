f7a5def48eb184ca729b15103ea1da587abf5344
Val <60014136+voffee@users.noreply.github.com>
2026-06-25 10:30:31 +0800

    js for user input works.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 
-const songTitle = document.querySelectorAll("#song-title");
+// Variable Declaration
+const songTitle = document.querySelector("#song-title");
+const artistName = document.querySelector("#artist");
+const songMinutes = document.querySelector("#songminutes");
+const songSeconds = document.querySelector("#songseconds");
+const form = document.querySelector(".song-entry");
 
-console.log(songTitle);
+// Event Listener Creation
+form.addEventListener("submit", queueHandler);
+
+function queueHandler(e) {
+    e.preventDefault();
+    console.log("Added to queue");
+
+    const songBundle = {
+        title : songTitle.value,
+        artist : artistName.value,
+        minutes : songMinutes.value,
+        seconds : songSeconds.value,
+    }
+
+    console.log(songBundle);
+}
