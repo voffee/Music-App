9b4f4a84c9057d4d7552192d1ab518363a61a66d
Val <60014136+voffee@users.noreply.github.com>
2026-06-26 15:11:16 +0800

    js bundler done, quq population done but working to get data from canonical queue.

--- js/app.js
+++ js/app.js
@@ -6,9 +6,17 @@ const songMinutes = document.querySelector("#songminutes");
 const songSeconds = document.querySelector("#songseconds");
 const form = document.querySelector(".song-entry");
 
+const queueContainer = document.querySelector(".queue-container");
+const songContainer = document.querySelector(".song-container");
+
+let idCount = 0;
+const canonicalQueue = [];
+
 // Event Listener Creation
 form.addEventListener("submit", queueHandler);
+console.log(songContainer);
 
+// Queue Handler Function
 function queueHandler(e) {
     e.preventDefault();
     console.log("Added to queue");
@@ -18,7 +26,32 @@ function queueHandler(e) {
         artist : artistName.value,
         minutes : songMinutes.value,
         seconds : songSeconds.value,
+        songID : idCount,
     }
 
+    idCount = idCount + 1;
+
+    bundler(songBundle);
+    queueConstructor(canonicalQueue);
     console.log(songBundle);
+    console.log(canonicalQueue);
+}
+
+// Song Bundler Function
+function bundler(passedSong) {
+    canonicalQueue.push(passedSong);
+}
+
+// Queue Constructor Function
+function queueConstructor(passedBundle) {
+    
+    songContainer.innerHTML = `
+        <p class="montserrat-small">${passedBundle.songID}</p>
+        <p class="montserrat-medium">${passedBundle.title}</p>
+        <p class="montserrat-small">${passedBundle.artist}</p>
+        <p class="montserrat-small">${passedBundle.minutes}: </p>
+        <p class="montserrat-small">${passedBundle.seconds}</p>
+        <input type="button" value="Move to top" class="montserrat-small">
+        <input type="button" value="Remove" class="montserrat-small">
+        `;
 }
