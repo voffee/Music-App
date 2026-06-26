db65002fd28e07c2500b585612f610f86872e9dc
Val <60014136+voffee@users.noreply.github.com>
2026-06-26 15:53:13 +0800

    queue works, left with index creation and button functionality.

--- js/app.js
+++ js/app.js
@@ -7,20 +7,18 @@ const songSeconds = document.querySelector("#songseconds");
 const form = document.querySelector(".song-entry");
 
 const queueContainer = document.querySelector(".queue-container");
-const songContainer = document.querySelector(".song-container");
+// const songContainer = document.querySelector(".song-container");
 
 let idCount = 0;
 const canonicalQueue = [];
 
 // Event Listener Creation
 form.addEventListener("submit", queueHandler);
-console.log(songContainer);
 
 // Queue Handler Function
 function queueHandler(e) {
     e.preventDefault();
     console.log("Added to queue");
-
     const songBundle = {
         title : songTitle.value,
         artist : artistName.value,
@@ -29,12 +27,10 @@ function queueHandler(e) {
         songID : idCount,
     }
 
-    idCount = idCount + 1;
-
     bundler(songBundle);
     queueConstructor(canonicalQueue);
-    console.log(songBundle);
-    console.log(canonicalQueue);
+
+    idCount = idCount + 1;
 }
 
 // Song Bundler Function
@@ -43,15 +39,22 @@ function bundler(passedSong) {
 }
 
 // Queue Constructor Function
-function queueConstructor(passedBundle) {
-    
-    songContainer.innerHTML = `
-        <p class="montserrat-small">${passedBundle.songID}</p>
-        <p class="montserrat-medium">${passedBundle.title}</p>
-        <p class="montserrat-small">${passedBundle.artist}</p>
-        <p class="montserrat-small">${passedBundle.minutes}: </p>
-        <p class="montserrat-small">${passedBundle.seconds}</p>
+function queueConstructor(passedData) {
+    const duplicatedCanonicalQueue = [...passedData];
+    queueContainer.innerHTML = ``;
+    duplicatedCanonicalQueue.forEach(element => {
+        const newSong = document.createElement('div');
+        newSong.classList.add('song-container');
+        newSong.innerHTML = `
+        <p class="montserrat-small">${element.songID}</p>
+        <p class="montserrat-medium">${element.title}</p>
+        <p class="montserrat-small">${element.artist}</p>
+        <p class="montserrat-small">${element.minutes}: </p>
+        <p class="montserrat-small">${element.seconds}</p>
         <input type="button" value="Move to top" class="montserrat-small">
         <input type="button" value="Remove" class="montserrat-small">
         `;
+        queueContainer.appendChild(newSong);
+    });
+    console.log(duplicatedCanonicalQueue[idCount]);
 }
