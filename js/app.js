74a3b14e361a35fb790c39c9b6f065b39013e988
Val <60014136+voffee@users.noreply.github.com>
2026-06-30 18:41:39 +0800

    now playing js done and remove song done

--- js/app.js
+++ js/app.js
@@ -7,13 +7,14 @@ const songSeconds = document.querySelector("#songseconds");
 const form = document.querySelector(".song-entry");
 
 const queueContainer = document.querySelector(".queue-container");
-// const songContainer = document.querySelector(".song-container");
+const nowPlayingContainer = document.querySelector(".now-playing-container");
 
 let idCount = 0;
 const canonicalQueue = [];
 
 // Event Listener Creation
 form.addEventListener("submit", queueHandler);
+queueContainer.addEventListener("click", buttonHandler);
 
 // Queue Handler Function
 function queueHandler(e) {
@@ -29,7 +30,7 @@ function queueHandler(e) {
 
     bundler(songBundle);
     queueConstructor(canonicalQueue);
-    nowPlayingConstructor(canonicalQueue[1]);
+    nowPlayingConstructor(canonicalQueue[0]);
 
     idCount = idCount + 1;
 }
@@ -55,14 +56,52 @@ function queueConstructor(passedData) {
                     <p class="montserrat-small">${element.minutes}: </p>
                     <p class="montserrat-small">${element.seconds}</p>
                 </div>
-                <input type="button" value="Move to top" class="montserrat-small">
-                <input type="button" value="Remove" class="montserrat-small">
+                <input type="button" value="Move to top" class="montserrat-small" data-action="move-to-top">
+                <input type="button" value="Remove" class="montserrat-small" data-action="remove">
         `;
+        newSong.dataset.musicid = songIndex;
         queueContainer.appendChild(newSong);
     });
-    console.log(duplicatedCanonicalQueue[idCount]);
+    // console.log(duplicatedCanonicalQueue[idCount]);
 }
 
+// Now Playing Constructor Function
 function nowPlayingConstructor(passedData) {
-    console.log(passedData);
+    nowPlayingContainer.innerHTML = `
+            <p class="opensans-small">Now Playing</p>
+            <p class="montserrat-medium">${passedData.title}</p>
+            <p class="montserrat-small">${passedData.artist}</p>
+            <p class="montserrat-small">${passedData.minutes}: </p>
+            <p class="montserrat-small">${passedData.seconds}</p>
+    `;
+}
+
+// Button Handler Function
+function buttonHandler(e) {
+    switch(e.target.dataset.action) {
+        case "move-to-top":
+            console.log("move to top");
+            moveSong(e.target.closest(".song-container"));
+
+            break;
+        case "remove":
+            // console.log("remove");
+            removeSong(e.target.closest(".song-container"));
+            queueConstructor(canonicalQueue);
+            nowPlayingConstructor(canonicalQueue[0]);
+            break;
+
+        default:
+            console.log("invalid click");
+    }
+}
+
+function removeSong(passedData) {
+    // console.log(passedData);
+    // passedData.dataset.musicid;
+    canonicalQueue.splice(passedData.dataset.musicid - 1, 1);
+}
+
+function moveSong(passedData) {
+    // canonicalQueue
 }
